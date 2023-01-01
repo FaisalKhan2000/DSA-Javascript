614a469d21f598d144178881823619950da9ea1a
Faisal Khan <iamfaisal.luv@gmail.com>
2023-01-01 23:05:57 +0530

    Adding Challenges Folder

---
+++ BIgO/Challenges/challenge1.js
@@ -0,0 +1,17 @@
+// Big O Calculation
+
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < array.length; i++) {
+    // O(n);
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+
+  return a; // O(1)
+}
+// 1 + 1 + 1 + 1n + 1n + 1n + 1n
+// BIG O(3 + 4n)
